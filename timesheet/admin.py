c1b01995106581beae82add9062255d9c608cf3b
lavmaxa <61774351+lavmaxa@users.noreply.github.com>
2021-06-25 01:44:08 +0800

    Add files via upload

---
+++ timesheet/admin.py
@@ -0,0 +1,19 @@
+
+from django.contrib import admin
+from .models import *
+
+admin.site.register(IMIT)
+admin.site.register(Physicists)
+admin.site.register(LAU)
+admin.site.register(PI)
+admin.site.register(MIEL)
+admin.site.register(INYAZ)
+admin.site.register(ISN)
+admin.site.register(BMB)
+admin.site.register(BIO)
+admin.site.register(GEOG)
+admin.site.register(HIST)
+admin.site.register(SAF)
+admin.site.register(SR)
+admin.site.register(PSY)
+admin.site.register(CHEM)
